b32f461fe3b66ca3af72bd7296beef2eb089335e
kerim.sisman <kerim.sisman.9@fntsoftware.com>
2024-09-17 13:20:27 +0300

    sleekPlanWebHook

--- src/main/java/com/scalefocus/digitalriver/Webhook.java
+++ src/main/java/com/scalefocus/digitalriver/Webhook.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.ws.rs.QueryParam;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
@@ -16,10 +17,22 @@ public class Webhook {
 
 	@PostMapping
 	public String hook(@RequestBody String requestBody) {
-		log.info("started. {}",Instant.now());
-		log.info("requestBody:[{}]",requestBody);
-		log.info("completed. {}",Instant.now());
+		log.info("started. {}", Instant.now());
+		log.info("requestBody:[{}]", requestBody);
+		log.info("completed. {}", Instant.now());
 		return "ok";
+	}
+
+	@PostMapping(path = "/sleekplan")
+	public String sleekplan(@QueryParam("key") String key, @RequestBody String requestBody) {
+		log.info("sleekplan-started. {}", Instant.now());
+
+		log.info("sleekplan-key:[{}]", key);
+
+		log.info("sleekplan-requestBody:[{}]", requestBody);
+
+		log.info("sleekplan-completed. {}", Instant.now());
+		return "sleekplan-ok";
 	}
 
 }
